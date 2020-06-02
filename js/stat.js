40875f198e890c90016a24d314075f37dc35099d
Viktoriia <viktoria.nosik@gmail.com>
2020-06-02 12:12:30 +0300

    add color for bar

--- js/stat.js
+++ js/stat.js
@@ -9,24 +9,26 @@ var BAR_GAP = 50;
 var BAR_WIDTH = 40;
 var BAR_HEIGHT_MAX = 150;
 var TEXT_FONT = '16px PT Mono';
-var TEXT_X = CLOUD_X + 3*GAP;
-var TEXT_Y = CLOUD_Y + 3*GAP;
+var TEXT_X = CLOUD_X + 3 * GAP;
+var TEXT_Y = CLOUD_Y + 3 * GAP;
 var TEXT_GAP = 20;
 var START_X = CLOUD_X + 50;
 var NAME_Y = 260;
 var BAR_Y = 240;
+var YOU_COLOR = 'rgba(255,0,0,1)';
 
-var renderCloud = function(ctx, x, y, color) {
+var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
 var renderText = function (ctx, x, y, text) {
   ctx.font = TEXT_FONT;
+  ctx.fillStyle = '#000';
   ctx.fillText(text, x, y);
-}
+};
 
-var getMaxElement = function(arr) {
+var getMaxElement = function (arr) {
   var maxElement = arr[0];
   for (var i = 0; i < arr.length; i++) {
     if (arr[i] > maxElement) {
@@ -36,7 +38,11 @@ var getMaxElement = function(arr) {
   return maxElement;
 };
 
-window.renderStatistics = function(ctx, players, times) {
+var getRandom = function () {
+  return Math.floor(Math.random() * 101);
+};
+
+window.renderStatistics = function (ctx, players, times) {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
 
@@ -50,8 +56,12 @@ window.renderStatistics = function(ctx, players, times) {
     var result = Math.floor(times[i]);
     var barHeight = BAR_HEIGHT_MAX * times[i] / maxTime;
     var x = START_X + (BAR_WIDTH + BAR_GAP) * i;
-    ctx.fillRect(START_X + (BAR_WIDTH + BAR_GAP) * i, BAR_Y, BAR_WIDTH, -barHeight);
     renderText(ctx, x, NAME_Y, players[i]);
-    renderText(ctx, x, BAR_Y - barHeight  - GAP, result);
+    renderText(ctx, x, BAR_Y - barHeight - GAP, result);
+    ctx.fillStyle = 'hsl(240, ' + getRandom() + '%, 50%)';
+    if (players[i] === 'Вы') {
+      ctx.fillStyle = YOU_COLOR;
+    }
+    ctx.fillRect(x, BAR_Y, BAR_WIDTH, -barHeight);
   }
 };
